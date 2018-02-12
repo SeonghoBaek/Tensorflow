fb8aaa79f341227e3a483e0dc24c6c9c80ea0730
major <seonghobaek@gmail.com>
2018-02-12 14:42:02 +0900

    Add XGBoost

--- Prognostic/RUL.py
+++ Prognostic/RUL.py
@@ -5,33 +5,42 @@ from sklearn.preprocessing import scale
 
 
 def windows(nrows, size):
-    start,step = 0, 2
+    start, step = 0, 2
     while start < nrows:
         yield start, start + size
         start += step
 
 
-def segment_signal(features,labels,window_size = 15):
-    segments = np.empty((0,window_size))
+def segment_signal(features, labels, window_size = 15):
+    segments = np.empty((0, window_size))
     segment_labels = np.empty((0))
     nrows = len(features)
-    for (start, end) in windows(nrows,window_size):
-        if(len(data.iloc[start:end]) == window_size):
+
+    print('Num row: ', nrows)
+
+    for (start, end) in windows(nrows, window_size):
+        if len(data.iloc[start:end]) == window_size:
             segment = features[start:end].T  #Transpose to get segment of size 24 x 15
             label = labels[(end-1)]
-            segments = np.vstack([segments,segment])
-            segment_labels = np.append(segment_labels,label)
+            segments = np.vstack([segments, segment])
+            segment_labels = np.append(segment_labels, label)
+
     segments = segments.reshape(-1,24,window_size,1) # number of features  = 24
     segment_labels = segment_labels.reshape(-1,1)
-    return segments,segment_labels
+    return segments, segment_labels
 
 
+# id,cycle,setting1,setting2,setting3,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21,RUL
+print('Read CSV')
 data = pd.read_csv("PHM08.csv")
-features = scale(data.iloc[:,2:26]) # select required columns and scale them
-labels = data.iloc[:,26] # select RUL
+print('Scale CSV')
+features = scale(data.iloc[:, 2:26])  # select required columns and scale them
+labels = data.iloc[:, 26]  # select RUL
 
-segments, labels = segment_signal(features,labels)
+print('Segmentation')
+segments, labels = segment_signal(features, labels)
 
+print('Prepare data')
 train_test_split = np.random.rand(len(segments)) < 0.70
 train_x = segments[train_test_split]
 train_y = labels[train_test_split]
@@ -72,10 +81,15 @@ total_batches = train_x.shape[0] // batch_size
 X = tf.placeholder(tf.float32, shape=[None, input_height, input_width, num_channels])
 Y = tf.placeholder(tf.float32, shape=[None, num_labels])
 
+print('Build Graph')
 c = apply_conv(X, kernel_height=24, kernel_width=4, num_channels=1, depth=8)
+print(c.get_shape().as_list())
 p = apply_max_pool(c, kernel_height=1, kernel_width=2, stride_size=2)
+print(p.get_shape().as_list())
 c = apply_conv(p, kernel_height=1, kernel_width=3, num_channels=8, depth=14)
+print(c.get_shape().as_list())
 p = apply_max_pool(c, kernel_height=1, kernel_width=2, stride_size=2)
+print(p.get_shape().as_list())
 
 shape = p.get_shape().as_list()
 flat = tf.reshape(p, [-1, shape[1] * shape[2] * shape[3]])
@@ -90,9 +104,10 @@ y_ = tf.add(tf.matmul(f, out_weights), out_biases)
 
 cost_function = tf.reduce_mean(tf.square(y_ - Y))
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost_function)
+init = tf.global_variables_initializer()
 
 with tf.Session() as session:
-    session.run(tf.global_variables_initializer())
+    session.run(init)
 
     print("Training set MSE")
     for epoch in range(training_epochs):
